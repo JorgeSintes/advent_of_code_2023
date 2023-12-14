676b6b023c7e8acc1ea4de182aa950f2b572cff4
JorgeSintes <jorge.sintes@gmail.com>
2023-12-14 18:52:03 +0100

    day10.2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -156,7 +156,125 @@ int construct_graph(Map &map, Node* start_node) {
         i++;
     }
     node->previous->next = start_node;
+    start_node->previous = node->previous;
     return i;
+}
+
+bool is_in_path(Node* start_node, int i, int j) {
+    Node* current = start_node;
+    if (current->i == i && current->j == j) {
+        return true;
+    }
+    current = current->next;
+    while (current != start_node) {
+        if (current->i == i && current->j == j) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+void solve_S(Map &map, Node* start_node) {
+    Node* next_node = start_node->next;
+    Node* previous_node = start_node->previous;
+    int next= -1, previous = -1;
+    if (next_node->i - start_node->i == -1) {
+        next = 0;
+    }
+    else if (next_node->j - start_node->j == 1) {
+        next = 1;
+    }
+    else if (next_node->i - start_node->i == 1) {
+        next = 2;
+    }
+    else if (next_node->j - start_node->j == -1) {
+        next = 3;
+    }
+    if (previous_node->i - start_node->i == -1) {
+        previous = 0;
+    }
+    else if (previous_node->j - start_node->j == 1) {
+        previous = 1;
+    }
+    else if (previous_node->i - start_node->i == 1) {
+        previous = 2;
+    }
+    else if (previous_node->j - start_node->j == -1) {
+        previous = 3;
+    }
+
+    if (next == 0) {
+        if (previous == 1) {
+            map[start_node->i][start_node->j] = 'L';
+        }
+        else if (previous == 3) {
+            map[start_node->i][start_node->j] = 'J';
+        }
+        else {
+            map[start_node->i][start_node->j] = '|';
+        }
+    }
+    else if (next == 1) {
+        if (previous == 0) {
+            map[start_node->i][start_node->j] = 'L';
+        }
+        else if (previous == 2) {
+            map[start_node->i][start_node->j] = 'F';
+        }
+        else {
+            map[start_node->i][start_node->j] = '-';
+        }
+    }
+    else if (next == 2) {
+        if (previous == 1) {
+            map[start_node->i][start_node->j] = 'F';
+        }
+        else if (previous == 3) {
+            map[start_node->i][start_node->j] = '7';
+        }
+        else {
+            map[start_node->i][start_node->j] = '|';
+        }
+    }
+    else if (next == 3) {
+        if (previous == 0) {
+            map[start_node->i][start_node->j] = 'J';
+        }
+        else if (previous == 2) {
+            map[start_node->i][start_node->j] = '7';
+        }
+        else {
+            map[start_node->i][start_node->j] = '-';
+        }
+    }
+}
+
+bool is_inside_path(Map &map, Node* start_node, int i, int j) {
+    if (is_in_path(start_node, i, j)) {
+        return false;
+    }
+    int no_crosses = 0;
+    char parallel_pipe = ' ';
+    for (int k = i; k >= 0; k--) {
+        if (is_in_path(start_node, k, j)) {
+            if (map[k][j] == '-') {
+                no_crosses++;
+            }
+            if (map[k][j] == 'J' || map[k][j] == 'L') {
+                parallel_pipe = map[k][j];
+            }
+            if (map[k][j] == '7' && parallel_pipe == 'L') {
+                no_crosses++;
+                parallel_pipe = ' ';
+            }
+            if (map[k][j] == 'F' && parallel_pipe == 'J') {
+                no_crosses++;
+                parallel_pipe = ' ';
+            }
+        }
+    }
+    return no_crosses % 2 == 1;
 }
 
 int main() {
@@ -165,6 +283,16 @@ int main() {
     Map map = parse_input(input);
     Node* start_node = find_start(map);
     int steps = construct_graph(map, start_node);
+    solve_S(map, start_node);
     cout << "Start node: " << start_node->i << " " << start_node->j << endl;
     cout << "Steps: " << steps/2 << endl;
+
+    int inside_area = 0;
+    for (int i = 0; i < map.size(); i++) {
+        for (int j = 0; j < map[0].size(); j++) {
+            inside_area += is_inside_path(map, start_node, i, j);
+        }
+    }
+
+    cout << "Inside area: " << inside_area << endl;
 }
